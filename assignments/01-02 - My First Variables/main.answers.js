5d685e3360b8b426b93e634dc5f8e6dd18c4bcea
Daniel Indictor <daniel.indictor@gmail.com>
2019-07-09 12:12:36 -0400

    Added boring variable practice.

---
+++ assignments/01-02 - My First Variables/main.answers.js
@@ -0,0 +1,6 @@
+// First open the console when viewing index.html in your browser to see the
+// effect of the following line:
+console.log("Hello, World!");
+
+// See how we put the text we want the computer to use in quotes.
+console.log("Hello! My name is Michael Whalen!")
