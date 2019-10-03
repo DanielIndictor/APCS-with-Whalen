a87b60b0a5ddfc1d759cbc013a3b25cbd6a3f486
Michael Whalen <michael.whalen@sitechhs.com>
2019-10-02 20:58:15 -0400

    query

--- arrow function exercise/app.js
+++ arrow function exercise/app.js
@@ -1,4 +1,4 @@
-const liList = Array.from(document.querySelectorAll('li'));
+/* const liList = Array.from(document.querySelectorAll('li'));
 //console.log(liList);
 
 liList.filter (el => el.textContent.includes('Flexbox'));
@@ -14,4 +14,21 @@ const filtered = liList.filter (el => el.textContent.includes('Flexbox'))
     return (parts[0] * 60) + parts[1];
 })
 .reduce((runningTotal, seconds) => runningTotal + seconds,0)
-console.log(filtered);
+console.log(filtered); */
+
+
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0);
+        //return the result
+        return `${newTitle.join(' ')}...)`;
+    } return title;
+}
+
+limitRecipeTitle('Pasta Primavera with spaghetti and meatballs blah blah blah');
